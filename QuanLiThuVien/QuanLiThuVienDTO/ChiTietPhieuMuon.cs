d6bb7e4f8ab357f1171c5bf24d2771660f31cac6
cylasion <16521339@gm.uit.edu.vn>
2018-04-20 23:35:48 +0700

    Thêm DTO
    Thêm các DTO dựa vào ER

---
+++ QuanLiThuVien/QuanLiThuVienDTO/ChiTietPhieuMuon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiThuVienDTO
+{
+    class ChiTietPhieuMuon
+    {
+        public string MaPM { get; set; }
+        public string MaSach { get; set;  }
+
+        /// <summary>
+        /// Tạo 1 đối tượng với các thuộc tính là ""
+        /// </summary>
+        public ChiTietPhieuMuon()
+        {
+            this.MaPM = "";
+            this.MaSach = "";
+        }
+    }
+}
---
+++ QuanLiThuVien/QuanLiThuVienDTO/ChiTietPhieuTra.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiThuVienDTO
+{
+    class ChiTietPhieuTra
+    {
+        public string MaPT { get; set; }
+        public string MaSach { get; set; }
+        public int TienPhatChoSachNay { get; set; }
+
+        /// <summary>
+        /// Tạo 1 chi tiết phiếu trả với các giá trị là "" và 0
+        /// </summary>
+        public ChiTietPhieuTra()
+        {
+            this.MaPT = "";
+            this.MaSach = "";
+            this.TienPhatChoSachNay = 0;
+        }
+    }
+}
