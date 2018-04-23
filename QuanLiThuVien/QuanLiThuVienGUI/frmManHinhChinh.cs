148fd04e791d02648e67dbb5549255304539d0e4
hoango7604 <hoango7604@gmail.com>
2018-04-23 11:42:02 +0700

    update lần 1
    update GUI màn hình chính

--- QuanLiThuVien/QuanLiThuVienGUI/frmManHinhChinh.cs
+++ QuanLiThuVien/QuanLiThuVienGUI/frmManHinhChinh.cs
@@ -17,7 +17,7 @@ namespace QuanLiThuVienGUI
             InitializeComponent();
         }
 
-        private void thêmBạnĐọcToolStripMenuItem_Click(object sender, EventArgs e)
+        private void thembandocToolStripMenuItem_Click(object sender, EventArgs e)
         {
             frmThemBanDoc f = new frmThemBanDoc();
             f.ShowDialog();
@@ -27,6 +27,26 @@ namespace QuanLiThuVienGUI
         {
             frmThemSach f = new frmThemSach();
             f.ShowDialog();
+        }
+
+        private void btnThemBanDoc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSuaThongTinBanDoc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoaBanDoc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tìmKiếmBạnĐọcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
