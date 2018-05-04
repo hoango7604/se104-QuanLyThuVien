203b0eb4cc96c66d7257a26275a53214e9599d19
Nguyễn Minh Hiếu <37030148+DatteBayo09@users.noreply.github.com>
2018-05-04 18:21:30 +0700

    Add files via upload

---
+++ QuanLiThuVien/cacloaidocgiaDAL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dbConnection;
+using QuanLiThuVienDTO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLiThuVienDAL
+{
+   public class cacloaidocgiaDAL
+    {
+          
+       private dbconnection conn;
+
+
+       public cacloaidocgiaDAL()
+       {
+           conn = new dbconnection(); 
+       }
+
+
+
+
+       public bool danhsachLoaiDG(List<cacloaidocgiaDTO> listLoaiDGDTO)
+       {
+
+           string query = string.Format("select* from [cacloaidocgia] ");
+           SqlParameter[] parm = new SqlParameter[1];
+
+           DataTable datatable = new DataTable();
+           datatable = conn.excuteNonQuery(query, parm);
+
+           //gan value trong datatable vao DTO 
+
+           foreach (DataRow dr in datatable.Rows)
+           {
+               cacloaidocgiaDTO cacloaiDGDTO = new cacloaidocgiaDTO();
+
+               cacloaiDGDTO.Cacloai = int.Parse(dr["loaidg"].ToString());
+              
+               listLoaiDGDTO.Add(cacloaiDGDTO);
+           }
+
+
+
+
+           return true;
+       }
+
+
+    }
+}
---
+++ QuanLiThuVien/quydinhDAL.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dbConnection;
+using QuanLiThuVienDTO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLiThuVienDAL
+{
+   public  class quydinhDAL
+    {
+       
+       private dbconnection conn;
+
+
+       public quydinhDAL()
+       {
+           conn = new dbconnection(); 
+       }
+        // hien thi list dto 
+
+       public bool listquydinh(List<quydinhDTO> lqđTO)
+       {
+
+
+           string query = string.Format("select * from [quydinh] ");
+
+           SqlParameter[] param = new SqlParameter[1]; 
+           DataTable dt= new DataTable ();
+
+           dt = conn.excuteNonQuery(query, param);
+
+           foreach (DataRow dr in dt.Rows)
+           {
+               quydinhDTO qdDTO = new quydinhDTO();
+
+               qdDTO.Tuoimax = int.Parse(dr["tuoimax"].ToString());
+               qdDTO.Tuoimin = int.Parse(dr["tuoimin"].ToString());
+               qdDTO.Songayduocmuon = int.Parse(dr["songayduocmuon"].ToString());
+               qdDTO.Sosachduocmuon = int.Parse(dr["sosachduocmuon"].ToString());
+               qdDTO.Kcnamxuatban = int.Parse(dr["kcnamxuatban"].ToString());
+
+               lqđTO.Add(qdDTO);
+
+           }
+
+
+           return true;
+       }
+
+
+       public bool suaQuyDinh(quydinhDTO qdDTO)
+       {
+           string query = string.Format("update [quydinh] set tuoimax =@tuoimax ,tuoimin=@tuoimin , songayduocmuon=@songayduocmuon ,sosachduocmuon=@sosachduocmuon ,kcnamxuatban =@kcnamxuatban where ROWNUM <=3");
+           
+
+          SqlParameter [] param= new SqlParameter [5];
+          param[0] = new SqlParameter("@tuoimax", SqlDbType.Int);
+          param[0].Value = Convert.ToString(qdDTO.Tuoimax);
+          param[1] = new SqlParameter("@tuoimin", SqlDbType.Int);
+          param[1].Value = Convert.ToString(qdDTO.Tuoimin);
+          param[2] = new SqlParameter("@songayduocmuon", SqlDbType.Int);
+          param[2].Value = Convert.ToString(qdDTO.Songayduocmuon);
+          param[3] = new SqlParameter("@sosachduocmuon", SqlDbType.Int);
+          param[3].Value = Convert.ToString(qdDTO.Sosachduocmuon);
+          param[4] = new SqlParameter("@kcnamxuatban", SqlDbType.Int);
+          param[4].Value = Convert.ToString(qdDTO.Kcnamxuatban);
+
+
+          conn.excuteNonQuery2(query, param);
+
+          return true;
+
+       }
+
+    }
+}
