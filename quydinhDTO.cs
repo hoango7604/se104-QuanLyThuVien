9191f03483ca3da2a863724a8713c357c1a93c95
Nguyễn Minh Hiếu <37030148+DatteBayo09@users.noreply.github.com>
2018-05-07 20:27:49 +0700

    Add files via upload

---
+++ quydinhDTO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace QuanLiThuVienDTO
+{
+    public class quydinhDTO
+    {
+        public quydinhDTO()
+        {
+
+        }
+        public quydinhDTO(int tuoimax, int tuoimin, int sosachduocmuon, int kcnamxuatban)
+        {
+            Tuoimax = tuoimax;
+
+            Tuoimin = tuoimin;
+            Songayduocmuon = songayduocmuon;
+
+
+            Kcnamxuatban = kcnamxuatban;
+        }
+
+        private int tuoimax;
+
+        public int Tuoimax
+        {
+            get { return tuoimax; }
+            set { tuoimax = value; }
+        }
+        private int tuoimin;
+
+        public int Tuoimin
+        {
+            get { return tuoimin; }
+            set { tuoimin = value; }
+        }
+        private int songayduocmuon;
+
+        public int Songayduocmuon
+        {
+            get { return songayduocmuon; }
+            set { songayduocmuon = value; }
+        }
+        private int sosachduocmuon;
+
+        public int Sosachduocmuon
+        {
+            get { return sosachduocmuon; }
+            set { sosachduocmuon = value; }
+        }
+        private int kcnamxuatban;
+
+        public int Kcnamxuatban
+        {
+            get { return kcnamxuatban; }
+            set { kcnamxuatban = value; }
+        } 
+ 
+
+
+    }
+}
