658dcdd9cb5ad647c1ff3a68bcf95cc6e875d6c9
Nguyễn Minh Hiếu <37030148+DatteBayo09@users.noreply.github.com>
2018-05-07 23:35:28 +0700

    Add files via upload

---
+++ QuanLiThuVien/QuanLiThuVienDTO/sachdatraDTO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiThuVienDTO
+{
+    public  class sachdatraDTO
+    {
+        public sachdatraDTO()
+        {
+
+        }
+
+        public sachdatraDTO(int masach , DateTime ngaytra )
+        {
+            Masach = masach;
+            Ngaytra = ngaytra; 
+
+        }
+
+        private int masach;
+
+        public int Masach
+        {
+            get { return masach; }
+            set { masach = value; }
+        }
+        private DateTime ngaytra;
+
+        public DateTime Ngaytra
+        {
+            get { return ngaytra; }
+            set { ngaytra = value; }
+        } 
+
+
+    }
+}
