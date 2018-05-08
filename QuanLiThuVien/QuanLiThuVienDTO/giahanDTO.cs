9199dce39ef976f47de520ff4e9d95cd1fde2712
Nguyễn Minh Hiếu <37030148+DatteBayo09@users.noreply.github.com>
2018-05-08 12:47:49 +0700

    Add files via upload

---
+++ QuanLiThuVien/QuanLiThuVienDTO/giahanDTO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiThuVienDTO
+{
+   public class giahanDTO
+    {
+       public giahanDTO()
+       {
+
+       }
+       public giahanDTO(int solangiahan , int masach )
+       {
+           Solangiahan = solangiahan;
+           Masach = masach;
+
+       }
+
+
+        private int solangiahan;
+
+        public int Solangiahan
+        {
+            get { return solangiahan; }
+            set { solangiahan = value; }
+        }
+        private int masach;
+
+        public int Masach
+        {
+            get { return masach; }
+            set { masach = value; }
+        } 
+
+    }
+}
