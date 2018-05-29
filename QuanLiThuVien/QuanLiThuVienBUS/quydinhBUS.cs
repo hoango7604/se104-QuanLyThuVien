4e5496430bbbf86ab911fb6a23685f2da18f8e23
hoango7604 <hoango7604@gmail.com>
2018-05-29 15:45:53 +0700

    update GUI

---
+++ QuanLiThuVien/QuanLiThuVienBUS/quydinhBUS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLiThuVienDAL;
+using QuanLiThuVienDTO;
+
+namespace QuanLiThuVienBUS
+{
+    public class quydinhBUS
+    {
+        public bool ListQuyDinh(quydinhDTO qdDTO)
+        {
+            try
+            {
+                quydinhDAL quydinhDAL = new quydinhDAL();
+                return quydinhDAL.listquydinh(qdDTO);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool SuaQuyDinh(quydinhDTO qdDTO)
+        {
+            try
+            {
+                quydinhDAL quydinhDAL = new quydinhDAL();
+                return quydinhDAL.suaQuyDinh(qdDTO);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
